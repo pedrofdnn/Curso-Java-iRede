a3f0a4804d2d21b39622863579ef5cfd324f00e1
Pedro Fernandes <pedrofdnn@gmail.com>
2026-01-31 19:19:52 -0300

    Iniciando repositorio

---
+++ src/main/java/com/mycompany/curso_java_irede/Curso_Java_iRede.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.curso_java_irede;
+
+/**
+ *
+ * @author pedro
+ */
+public class Curso_Java_iRede {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
