61567c599a4d66b43f57f745d4fd09930ed169c3
pedro <pedro@PF-HOME-LX>
2026-02-06 00:36:55 -0300

    Continuação.

--- src/main/java/Exercicios_Modulo_1/Exercicio_09.java
+++ src/main/java/Exercicios_Modulo_1/Exercicio_09.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Exercicios_Modulo_1;
 
-/**
- *
- * @author pedro
- */
+//  9. Faça um programa que leia o nome completo de uma pessoa e mostre o primeiro
+//     nome e o último nome separadamente.
+//
+import java.util.Scanner;
+
 public class Exercicio_09 {
-    
+
+    public static void main(String[] args) {
+
+        Scanner impuntName = new Scanner(System.in);
+
+        System.out.print("Digite o Seu nome Completo:");
+        String receive = impuntName.nextLine();
+
+        String[] listNames = {receive};
+
+        for (String name : listNames) {
+            String firtsLetter = name.substring(0, 1).toUpperCase();
+            String secondLetter = name.substring(1);
+
+            System.out.println("Seu nome completo é: "
+                    + firtsLetter + "" + secondLetter);
+
+        }
+    }
 }
