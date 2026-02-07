60989e1b86b1ab9318f0af069ee7d57b5aaae2d5
pedro <pedro@PF-HOME-LX>
2026-02-07 12:54:46 -0300

    Concluido.

--- src/main/java/Exercicios_Modulo_1/Exercicio_10.java
+++ src/main/java/Exercicios_Modulo_1/Exercicio_10.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Exercicios_Modulo_1;
 
-/**
- *
- * @author pedro
- */
+//10. Escreva um programa que leia uma frase e conte quantas palavras ela possui
+//(considere as palavras separadas por espaços).
+//
+import java.util.Scanner;
+
 public class Exercicio_10 {
-    
+
+    public static void main(String[] args) {
+        Scanner impuntPhrase = new Scanner(System.in);
+
+        System.out.print("Digite uma frase:");
+        String receive = impuntPhrase.nextLine();
+
+        String phrase = receive;
+
+        String[] countPhrase = phrase.split(" ");
+
+        int contador = 0;
+        String result = "";
+
+        for (String letter : countPhrase) {
+
+            if (!letter.isEmpty()) {
+                contador++;
+
+                String firtsLetter = letter.substring(0, 1).toUpperCase();
+                String secondLetter = letter.substring(1);
+
+                result += firtsLetter + secondLetter + " ";
+            }
+        }
+        System.out.println("Aqui esta sua frase: " + result.trim() + ".");
+        System.out.print("A frase possui: " + contador + ", Palavras. ");
+    }
 }
